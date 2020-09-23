072c874c17b3fa1accb71effd7f155b6b2500d55
IceSentry <c.giguere42@gmail.com>
2020-09-22 20:30:21 -0400

    add preact

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,3 +1,5 @@
+import { h, render, Fragment } from 'preact'
+
 const changeColor = document.getElementById('changeColor')
 chrome.storage.sync.get('color', (data) => {
   if (changeColor) {
@@ -9,12 +11,25 @@ chrome.storage.sync.get('color', (data) => {
 if (changeColor) {
   changeColor.onclick = (element) => {
     const color = (element?.target as HTMLInputElement).value
-    console.log(element.target)
+    console.log(`setting new color ${color}`)
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       // @ts-ignore
       chrome.tabs.executeScript(tabs[0].id, {
-        code: 'document.body.style.backgroundColor = "' + color + '";',
+        code: `document.body.style.backgroundColor = ${color};`,
       })
     })
   }
 }
+
+const App = () => (
+  <Fragment>
+    <h1>Hello from Preact and Typescript!</h1>
+    <div id="buttonDiv"></div>
+    <div>
+      <p>Choose a different background color!</p>
+    </div>
+    <button id="changeColor"></button>
+  </Fragment>
+)
+
+render(<App />, document.getElementById('root') as Element)
