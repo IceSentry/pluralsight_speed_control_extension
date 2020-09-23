f1861a49a361ceec862f48365e6f68f78868d448
IceSentry <c.giguere42@gmail.com>
2020-09-23 09:46:28 -0400

    remove ColorPicker

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,37 +1,10 @@
-/*global chrome*/
-
 import { h, render, Fragment } from 'preact'
-import { useEffect, useState } from 'preact/hooks'
-import { ColorPicker } from './components/ColorPicker'
 import { SpeedController } from './components/SpeedController'
 import { ReloadButton } from './components/ReloadButton'
 
 const App = () => {
-  const [color, setColor] = useState('')
-
-  useEffect(() => {
-    chrome.storage.sync.get('color', (data) => {
-      setColor(data.color)
-    })
-  }, [])
-
-  const onClick = () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const tabId = tabs[0].id as number
-      chrome.tabs.executeScript(tabId, {
-        code: `document.body.style.backgroundColor = '${color}';`,
-      })
-    })
-  }
-
   return (
     <Fragment>
-      <ColorPicker setColor={setColor} />
-      <button
-        onClick={onClick}
-        value={color}
-        style={{ backgroundColor: color }}
-      />
       <SpeedController />
       <ReloadButton />
     </Fragment>
